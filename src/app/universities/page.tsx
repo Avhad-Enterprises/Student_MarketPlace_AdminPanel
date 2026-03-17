ec48c3c45a65f6459d74540c8eb8e09b60b6355e
Prathamesh Devhare <devharep7@gmail.com>
2026-03-18 00:09:14 +0530

    fix: UniversitiesPage

--- src/app/universities/page.tsx
+++ src/app/universities/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { AdminLayout } from "@/components/AdminLayout";
 import { UniversitiesOverviewPage } from "@/components/UniversitiesOverviewPage";
 
 export default function UniversitiesPage() {
-    return (
-        <AdminLayout activePage="universities-list">
-            <UniversitiesOverviewPage />
-        </AdminLayout>
-    );
+  return (
+    <AdminLayout activePage="universities-list">
+      <UniversitiesOverviewPage />
+    </AdminLayout>
+  );
 }
