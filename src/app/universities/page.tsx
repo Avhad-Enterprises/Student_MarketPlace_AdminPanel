12b9b1a1305713ef028e71d73793190c478c8f1f
swapnilsonawane2237-hue <swapnilsonawane2237@gmail.com>
2026-03-18 10:28:06 +0530

    chnages for build

--- src/app/universities/page.tsx
+++ src/app/universities/page.tsx
@@ -2,13 +2,16 @@
 
 export const dynamic = "force-dynamic";
 
+import { Suspense } from 'react';
 import { AdminLayout } from "@/components/AdminLayout";
 import { UniversitiesOverviewPage } from "@/components/UniversitiesOverviewPage";
 
 export default function UniversitiesPage() {
   return (
     <AdminLayout activePage="universities-list">
-      <UniversitiesOverviewPage />
+      <Suspense fallback={<div>Loading Universities...</div>}>
+        <UniversitiesOverviewPage />
+      </Suspense>
     </AdminLayout>
   );
 }
